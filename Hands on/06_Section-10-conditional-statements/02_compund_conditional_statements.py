c2991538848d852d1e165424f987089141621a19
Sandeep Tanjore <sandeep.tanjore@mcgill.ca>
2026-06-04 13:45:21 -0400

    Started hands-on/challenges

---
+++ Hands on/06_Section-10-conditional-statements/02_compund_conditional_statements.py
@@ -0,0 +1,11 @@
+a= 5
+b=7
+c=3
+
+if a > b and a > c:
+    print("a is the greatest")
+if b > a and b > c:
+    print("b is the greatest")
+if c > a and c > b:
+    print("c is the greatest")  
+    
---
+++ Hands on/07_Section-11-challenges-using-conditionals/1_odd_even.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a number:"))
+
+if n%2 == 0:
+    print("The given number,", n, "is even")
+else:
+    print("The given number,", n, "is odd")
+
