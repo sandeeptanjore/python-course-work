c67322ece9be8e0d4beae21c656db585d8d8dc52
Sandeep <your-email@example.com>
2026-07-12 12:58:03 -0400

    Finished Strings

---
+++ Hands on/13_Section-17-strings/5_string_operations.py
@@ -0,0 +1,33 @@
+#Concatenation operator
+s1= 'wel'
+s2= 'come'
+
+s3= s1+s2
+print(s3)
+
+# Repetition
+
+s4='x'
+s5= s4*10
+print(s5)
+
+# Membership
+s6 ='abcdef'
+
+print('a' in s6)
+print('cde' in s6)
+print('z' in s6)
+print('z' not in s6)
+
+# String comparison
+
+variable1 ='apply'
+variable2 = 'apple'
+variable3= 'data'
+variable4 ='Data'
+
+print(variable1> variable2)
+print(variable1< variable2)
+
+print(variable3 < variable4) # false
+print(variable3>variable4) # true
